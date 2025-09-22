7f1c551d5315d0c0a72fccdf66977d757cb0487b
gadangbarubah <47277799-gadangbarubah@users.noreply.replit.com>
2025-09-22 23:21:16 +0000

    Restored to '0e06677c83b89aaae7f11d3d7215b8a9d53bda3b'
    Replit-Restored-To: 0e06677c83b89aaae7f11d3d7215b8a9d53bda3b

--- client/src/components/UniPage.tsx
+++ client/src/components/UniPage.tsx
@@ -9,7 +9,6 @@ import SEOHead from './SEOHead';
 import Mascot from './Mascot';
 import { BacklinksFooter } from './BacklinksFooter';
 import { StructuredData } from './StructuredData';
-import { PhotoPopup } from './PhotoPopup';
 import restaurantBgImage from '@assets/DSC03165_1758566711557.jpg';
 import rendangKiloanImage from '@assets/DSC02799_1758569186868.jpg';
 
@@ -245,9 +244,6 @@ export default function UniPage() {
       
       {/* Footer dengan Backlinks untuk SEO */}
       <BacklinksFooter />
-      
-      {/* Photo Upload Popup */}
-      <PhotoPopup />
     </div>
   );
 }
